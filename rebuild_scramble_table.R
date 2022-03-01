392e0b126cc55d60d88fa371e518811206680541
ylc24601 <95523578+ylc24601@users.noreply.github.com>
2022-03-01 09:57:01 +0800

    Update rebuild_scramble_table.R
    for sharing

--- rebuild_scramble_table.R
+++ rebuild_scramble_table.R
@@ -1,4 +1,3 @@
-library(glue)
 library(stringr)
 
 string1 = "1822631341052061172282914101911251223133814615271651718181193420402112223123282433253262127322842993029311632243335341535303673737381739364039"
@@ -9,7 +8,7 @@ rebuild_scramble_table <-function(string, num){
   for (i in num:1) {
     # cat("i = ", i, "\n")
     # cat("string_remain = ", string, "\n")
-    pattern = paste0(glue("{i}"), "(?=([0-9]{1,2}))")
+    pattern = paste0(i, "(?=([0-9]{1,2}))")
     len = nchar(string)
     matches <- str_match_all(substr(string, len - 3, len), pattern)
     # cat("possible matches: \n")
@@ -24,8 +23,7 @@ rebuild_scramble_table <-function(string, num){
     match_len = sum(nchar(match))
     # cat("characters to trim from the end: ", match_len, '\n')
     string = substr(string, 1, len - match_len)
-    # cat("--end--\n")
-    # cat(" \n")
+    # cat("--end--\n\n")
 
   }
    match_list
